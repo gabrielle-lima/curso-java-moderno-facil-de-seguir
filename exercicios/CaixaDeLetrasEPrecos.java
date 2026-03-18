ab58b364cce2b0565048c714f5e491eda48ca7d6
gabizinha12 <gaibou_lima@hotmail.com>
2026-03-18 14:18:24 -0300

    feat: aula de generics, refazer exercicio de caixa de letras e precos

--- exercicios/CaixaDeLetrasEPrecos.java
+++ exercicios/CaixaDeLetrasEPrecos.java
@@ -1,44 +1,32 @@
 class Caixa<T> {
-    private T elemento;
+    private T[] elementos;
     private int capacidade;
-    private int indice;
 
+    @SuppressWarnings("unchecked")
     public Caixa(int capacidade) {
         this.capacidade = capacidade;
+        this.elementos = (T[]) new Object[capacidade];
     }
 
     public int getCapacidade() {
         return capacidade;
     }
 
-    public int getIndice() {
-        return indice;
-    }
-
-    public void setIndice(int indice) {
-        this.indice = indice;
-    }
-
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
 
-    public T getElemento() {
-        return elemento;
-    }
-
-    public T setElemento(T elemento) {
-        return this.elemento = elemento;
-    }
-
     public void adicionar(T elemento, int indice) {
-            this.indice = indice;
-            setElemento(elemento);
+        if (indice >= 0 && indice < capacidade) {
+            this.elementos[indice] = elemento;
+        }
     }
 
     public T obter(int indice) {
-        Caixa<T> caixa = new Caixa<>(10);
-        return  this.elemento;
+        if (indice >= 0 && indice < capacidade) {
+            return this.elementos[indice];
+        }
+        return null;
     }
 
 }
@@ -47,8 +35,8 @@ class Caixa<T> {
 public class CaixaDeLetrasEPrecos {
     public static void main(String[] args) {
         Caixa<Double> caixa = new Caixa<>(2);
-        caixa.adicionar(20.50, 1);
-        caixa.adicionar(9.00, 2);
+        caixa.adicionar(20.50, 0);
+        caixa.adicionar(9.00, 1);
 
 
         System.out.println(caixa.obter(0));
